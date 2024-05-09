1f5f3586a2f9e6dc11b5a327e684c52334ad4ae6
AmoDinho <u14284783@tuks.co.za>
2024-05-09 15:59:01 +0200

    created event sources

---
+++ backend/events.ts
@@ -0,0 +1,10 @@
+const eventSources = {
+  warehouse_storage_object_put_handler: 'warehouse.storage.object_put_handler',
+};
+
+const eventDetailTypes = {
+  run_pipeline: '',
+  SourceDealershipFull: '',
+};
+
+export { eventDetailTypes, eventSources };
