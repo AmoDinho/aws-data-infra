6fbb8c55212047d0e204f3190d4914c5ef1c06eb
AmoDinho <u14284783@tuks.co.za>
2024-05-13 11:01:56 +0200

    made both schemas

---
+++ backend/stacks/warehouse/glue/schemas/cleanedDealership.ts
@@ -0,0 +1,90 @@
+const cleanedDealershipSchema = [
+  {
+    name: 'pk',
+    type: 'string',
+  },
+  {
+    name: 'sk',
+    type: 'string',
+  },
+  {
+    name: 'car_id',
+    type: 'string',
+  },
+  {
+    name: 'model',
+    type: 'string',
+  },
+  {
+    name: 'title',
+    type: 'string',
+  },
+  {
+    name: 'make',
+    type: 'string',
+  },
+  {
+    name: 'condition',
+    type: 'string',
+  },
+  {
+    name: 'price',
+    type: 'decimal(38,0)',
+  },
+  {
+    name: 'year',
+    type: 'string',
+  },
+  {
+    name: 'transmission',
+    type: 'string',
+  },
+  {
+    name: 'sold_by',
+    type: 'string',
+  },
+  {
+    name: 'user_id',
+    type: 'string',
+  },
+  {
+    name: 'dealership_id',
+    type: 'string',
+  },
+  {
+    name: 'dealership_name',
+    type: 'string',
+  },
+  {
+    name: 'dealership_lat',
+    type: 'decimal(38,0)',
+  },
+  {
+    name: 'dealership_lng',
+    type: 'decimal(38,0)',
+  },
+  {
+    name: 'created_at',
+    type: 'string',
+  },
+  {
+    name: 'updated_at',
+    type: 'string',
+  },
+  {
+    name: 'milage',
+    type: 'decimal(38,0)',
+  },
+  {
+    name: 'seller_info_name',
+    type: 'string',
+  },
+  {
+    name: 'seller_info_surname',
+    type: 'string',
+  },
+  {
+    name: 'seller_info_number',
+    type: 'decimal(38,0)',
+  },
+];
