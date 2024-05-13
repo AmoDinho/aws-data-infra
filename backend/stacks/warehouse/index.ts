b7185efdcb6785c1560b909815c44d51ec51dee8
AmoDinho <u14284783@tuks.co.za>
2024-05-13 13:31:56 +0200

    setting up things

--- backend/stacks/warehouse/index.ts
+++ backend/stacks/warehouse/index.ts
@@ -1,13 +1,15 @@
 import { StackContext } from 'sst/constructs';
 import { IWarehouseBucket, WarehouseBucket } from './storage';
-
+import { IGlue, Glue } from './glue';
 export interface IWarehouse {
   Warehouse: IWarehouseBucket;
+  Glue: IGlue;
 }
 export const Warehouse = (stackContext: StackContext): IWarehouse => {
   const storage = WarehouseBucket(stackContext);
-
+  const glue = Glue(stackContext, storage);
   return {
     Warehouse: storage,
+    Glue: glue,
   };
 };
