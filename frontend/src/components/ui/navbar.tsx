3b718e24828ad5b6736dcfaaf958a39e59c6c69e
AmoDinho <u14284783@tuks.co.za>
2024-05-23 16:21:44 +0200

    created navbar

---
+++ frontend/src/components/ui/navbar.tsx
@@ -0,0 +1,9 @@
+const NavBar = (): React.JSX.Element => (
+  <header className="bg-gray-900 text-white py-4 px-6 md:px-8">
+    <div className="container mx-auto flex items-center justify-between">
+      <h1 className="text-2xl font-bold">Car Dealership</h1>
+    </div>
+  </header>
+);
+
+export default NavBar;
