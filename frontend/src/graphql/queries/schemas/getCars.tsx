8e904325f558ad0de79fd1b309d9f163c9fc85e1
AmoDinho <u14284783@tuks.co.za>
2024-05-23 10:59:33 +0200

    added schema

--- frontend/src/graphql/queries/schemas/getCars.tsx
+++ frontend/src/graphql/queries/schemas/getCars.tsx
@@ -1,7 +1,27 @@
 import { gql } from '@apollo/client';
 
 export const GET_CARS = gql`
-{
-
-}
+  {
+    getCars {
+      car_id
+      condition
+      created_at
+      dealership_id
+      dealership_lat
+      dealership_lng
+      dealership_name
+      make
+      milage
+      model
+      pk
+      price
+      sk
+      sold_by
+      title
+      transmission
+      updated_at
+      user_id
+      year
+    }
+  }
 `;
---
+++ frontend/src/types/index.d.ts
@@ -0,0 +1,21 @@
+export interface ICar {
+  pk: string;
+  sk: string;
+  car_id: string;
+  model: string;
+  title: string;
+  make: string;
+  condition: string;
+  price: number;
+  year: string;
+  transmission: string;
+  sold_by: string;
+  user_id: string;
+  dealership_id: string;
+  dealership_name: string;
+  dealership_lat: number;
+  dealership_lng: number;
+  created_at: string;
+  updated_at: string;
+  milage: number;
+}
