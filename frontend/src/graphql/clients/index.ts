34d5a314252995b7443a2a064ff3a7e04e23fe89
AmoDinho <u14284783@tuks.co.za>
2024-05-23 10:51:47 +0200

    added graphql endpoint

--- frontend/src/graphql/clients/index.ts
+++ frontend/src/graphql/clients/index.ts
@@ -1,9 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const mainClient = () =>
-  new ApolloClient({
-    uri: '',
-    cache: new InMemoryCache(),
-  });
+const mainClient = new ApolloClient({
+  uri: import.meta.env.DEALERSHIPS_API_URL,
+  cache: new InMemoryCache(),
+});
 
 export { mainClient };
